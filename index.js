6327645446d52a84e3fcc5061f83318cba404d8e
nicola <me@nicola.io>
2016-04-17 00:59:17 -0400

    converting to es6

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-var util = require('util')
-var LdpStore = require('rdf-store-ldp')
-var SimpleRDFLite = require('./lite')
+'use strict'
 
-function SimpleRDF (context, iri, graph, store) {
-  if (!(this instanceof SimpleRDF)) {
-    return new SimpleRDF(context, iri, graph, store)
+const LdpStore = require('rdf-store-ldp')
+const lite = require('./lite')
+
+class SimpleRDF extends lite.SimpleRDF {
+  constructor (context, iri, graph, store) {
+    super(context, iri, graph)
+
+    this._store = store || new LdpStore()
   }
 
-  SimpleRDFLite.call(this, context, iri, graph)
+  child (iri) {
+    return new SimpleRDF(this._context, iri, this._graph, this._store)
+  }
 
-  this._store = store || new LdpStore()
+  get (iri, options) {
+    if (typeof iri !== 'string') {
+      options = iri
+      iri = null
+    }
+
+    if (iri) {
+      this.iri(iri)
+    }
+
+    return this._store
+      .graph(this._iri.toString(), null, options)
+      .then((graph) => {
+        this.graph(graph)
+
+        return this
+      })
+  }
+
+  save (iri, options) {
+    if (typeof iri !== 'string') {
+      options = iri
+      iri = null
+    }
+
+    if (iri) {
+      this.iri(iri)
+    }
+
+    return this._store
+      .add(this._iri.toString(), this._graph, null, options)
+      .then(() => {
+        return this
+      })
+  }
 }
 
-util.inherits(SimpleRDF, SimpleRDFLite)
-
-SimpleRDF.prototype.child = function (iri) {
-  return new SimpleRDF(this._context, iri, this._graph, this._store)
+module.exports = function (context, iri, graph, store) {
+  return new SimpleRDF(context, iri, graph, store)
 }
 
-SimpleRDF.prototype.get = function (iri, options) {
-  var self = this
-
-  if (typeof iri !== 'string') {
-    options = iri
-    iri = null
-  }
-
-  if (iri) {
-    self.iri(iri)
-  }
-
-  return self._store.graph(self._iri.toString(), null, options).then(function (graph) {
-    self.graph(graph)
-
-    return self
-  })
-}
-
-SimpleRDF.prototype.save = function (iri, options) {
-  var self = this
-
-  if (typeof iri !== 'string') {
-    options = iri
-    iri = null
-  }
-
-  if (iri) {
-    self.iri(iri)
-  }
-
-  return self._store.add(self._iri.toString(), self._graph, null, options).then(function () {
-    return self
-  })
-}
-
-SimpleRDF.isArray = SimpleRDFLite.isArray
-
-module.exports = SimpleRDF
+module.exports.isArray = lite.isArray
+module.exports.SimpleRDF = SimpleRDF
